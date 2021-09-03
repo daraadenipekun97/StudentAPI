7ceae59e05aae7db432079c1f2fc0b1ab29d6f16
daraadenipekun <daraadenipekun@gmail.com>
2021-09-03 10:56:03 +0100

    Added StudentDb Context install necessary EntityFrameWork Packages and added connection property in appsettings,json

---
+++ StudentAPI/Data/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Data
+{
+    public class StudentDbContext : DbContext
+    {
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
