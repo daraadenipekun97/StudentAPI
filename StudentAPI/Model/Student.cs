9e3d48b56505268babf11e8fbd4b30e12623fedc
daraadenipekun <daraadenipekun@gmail.com>
2021-09-03 09:33:38 +0100

    Created StudentAPI Model

---
+++ StudentAPI/Model/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Model
+{
+    public class Student
+    {
+        [Key]
+
+        public int StudentId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+
+        public string StudentName { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+
+        public string DateOfBirth { get; set; }
+
+        [Column(TypeName = "nvarchar(7)")]
+
+        public string Gender { get; set; }
+
+    }
+}
